fbb82526c0ff8660827273fa9ab1dd214646b291
Peter Roome <pete@lostmy.name>
2018-01-21 15:55:14 +0000

    Append product promotions logic and specs.

--- product.rb
+++ product.rb
@@ -4,8 +4,6 @@ class Product
   PRODUCTS = YAML.load(File.read("config/products.yml")
   ) unless defined?(PRODUCTS)
 
-  attr_accessor :product_code, :attributes
-
   def self.find(product_code)
     PRODUCTS.find do |product|
       product["product_code"] == product_code
